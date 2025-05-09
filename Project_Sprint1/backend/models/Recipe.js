ed1254a95255d45f751811cb33601d0b713cbe3b
Mohib Ullah Iftikhar <165938059+MohibIftikhar@users.noreply.github.com>
2025-05-09 21:15:36 +0500

    Update Recipe.js

--- Project_Sprint1/backend/models/Recipe.js
+++ Project_Sprint1/backend/models/Recipe.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ingredientSchema = new mongoose.Schema({
   name: { type: String, required: true },
   quantity: { type: String, required: true },
-  unit: { type: String, default: '', required: true }
+  unit: { type: String, default: '  ', required: true }
 });
 
 
